acd14c2685c8878d53944ee15e050fef676b369a
AALabonya <1aklimakter@gmail.com>
2024-06-12 21:13:37 +0600

    create sendResponse

--- src/app/middlewares/validateRequest.ts
+++ src/app/middlewares/validateRequest.ts
@@ -3,7 +3,6 @@ import { AnyZodObject } from 'zod';
 import catchAsync from '../../utils/catchAsync';
 
 
-
 const validateRequest = (schema: AnyZodObject) => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     await schema.parseAsync({
